81423643bbb0a3b886b333b21f8e8875d4fac80f
jeon-stat <jeon981118@gmail.com>
2026-05-22 03:44:39 +0900

    Update question_box.py

--- views/question_box.py
+++ views/question_box.py
@@ -1,4 +1,7 @@
 # views/question_box.py
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 
 from components import render_sidebar
